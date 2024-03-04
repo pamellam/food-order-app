2add106de6e70eefe65f644ae102268dae852519
pamellam <38889625+pamellam@users.noreply.github.com>
2024-03-04 09:43:19 +0200

    added a custom input component, handling form submission and validation, sendint a post request with order data

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,72 @@
+import { useContext } from 'react';
+
+import Modal from './UI/Modal';
+import CartContext from '../store/CartContext';
+import Input from './UI/Input';
+import Button from './UI/Button';
+import UserProgressContext from '../store/UserProgressContext';
+import { currencyFormatter } from '../util/formattting';
+
+const Checkout = () => {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Access data by using FormData API
+    const formData = new FormData(e.target);
+    const customerData = Object.fromEntries(formData.entries()); // {email: test@example.com }
+
+    // send the data to the backend
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+
+    // Reset the form or close the modal after successful submission
+    e.target.reset();
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+        <Input label="Full Name" id="name" type="text" />
+        <Input label="E-mail Address" id="email" type="email" />
+        <Input label="Street" id="street" type="text" />
+        <div className="control-row">
+          <Input label="Postal Code" id="postal-code" type="text"></Input>
+          <Input label="City" id="city" type="text"></Input>
+        </div>
+
+        <p className="modal-actions">
+          <Button type="button" textOnly onClick={handleClose}>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
+    </Modal>
+  );
+};
+
+export default Checkout;
